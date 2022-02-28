9e104086945bba806b750b35ebb24bf574246a1e
Nathan <PhantonQuad@users.noreply.github.com>
2022-02-28 17:53:48 +0000

    backup

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-datepicker/dist/react-datepicker.css";
 
-app.get("/cors", (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  res.send({ msg: "This has CORS enabled 🎈" });
-});
-
 function Navb() {
   return (
     <>
