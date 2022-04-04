c80ee4fa4528d3e9dd86e10a91e4df365a0abf3d
Nathan <PhantonQuad@users.noreply.github.com>
2022-04-04 12:21:08 +0100

    backup after deploy gh-pages

---
+++ build/precache-manifest.a217ba1d0631e4b85e90c92183f5de83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda3270965bf9dd045f593c16434ecee",
+    "url": "/Starter-Helium-API/index.html"
+  },
+  {
+    "revision": "c32b84e12b62275a57fb",
+    "url": "/Starter-Helium-API/static/css/2.edc52e77.chunk.css"
+  },
+  {
+    "revision": "e6e6e8996ff478909551",
+    "url": "/Starter-Helium-API/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "c32b84e12b62275a57fb",
+    "url": "/Starter-Helium-API/static/js/2.35ecd7d6.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/Starter-Helium-API/static/js/2.35ecd7d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e6e8996ff478909551",
+    "url": "/Starter-Helium-API/static/js/main.f2872cc7.chunk.js"
+  },
+  {
+    "revision": "0072d500c35481a58d96",
+    "url": "/Starter-Helium-API/static/js/runtime-main.a1069441.js"
+  }
+]);
